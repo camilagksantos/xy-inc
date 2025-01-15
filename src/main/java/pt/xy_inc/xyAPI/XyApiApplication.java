5dbc3af75189aaf27b93ed216eea0c0df634a140
Camila Kfouri <kfouri.camila@gmail.com>
2025-01-15 15:31:45 +0000

    Inicializando o Projeto

---
+++ src/main/java/pt/xy_inc/xyAPI/XyApiApplication.java
@@ -0,0 +1,13 @@
+package pt.xy_inc.xyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pt/xy_inc/xyAPI/XyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.xy_inc.xyAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XyApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
